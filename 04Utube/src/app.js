8d4e5aad08d7274449d7452842df7c18f96fbad5
Guru924 <guruprasaddas924@gmail.com>
2024-08-03 20:52:15 +0530

    created utils files

--- 04Utube/src/app.js
+++ 04Utube/src/app.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true
+}))
+app.use(express.json({limit: '16kb'})) //recieve form data
+app.use(express.urlencoded({extended: true, limit: '16kb'})) // take data from url
+app.use(express.static("public")) //store public files in server
+app.use(cookieParser())
 
 export {app}
--- 04Utube/src/index.js
+++ 04Utube/src/index.js
@@ -7,6 +7,18 @@ dotenv.config({
     path: './env'
 })
 
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION FAILED !!!",err);
+    
+})
+
 
 
 
