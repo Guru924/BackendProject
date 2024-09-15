162259cdca9ea23c7e67461482ba63ce4a70d4e9
Guru924 <guruprasaddas924@gmail.com>
2024-09-15 09:30:31 +0530

    add vedio router

--- 04Utube/src/app.js
+++ 04Utube/src/app.js
@@ -17,8 +17,10 @@ app.use(cookieParser())
 //routes import
 
 import userRouter from "./routes/user.routes.js"
+import videoRouter from "./routes/video.routes.js"
 
 //routes declartion
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/vedios",videoRouter)
 
 export {app}
