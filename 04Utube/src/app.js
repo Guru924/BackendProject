c6d6c1daabc95adde782939a067618d0b399f7f9
Guru924 <guruprasaddas924@gmail.com>
2024-09-16 19:41:27 +0530

    add comment route

--- 04Utube/src/app.js
+++ 04Utube/src/app.js
@@ -19,10 +19,12 @@ app.use(cookieParser())
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
+import commentRouter from "./routes/comment.routes.js"
 
 //routes declartion
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/vedios",videoRouter)
 app.use("/api/v1/tweets",tweetRouter)
+app.use("/api/v1/comments", commentRouter)
 
 export {app}
